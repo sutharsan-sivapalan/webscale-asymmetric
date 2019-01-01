32706a86103ae44e9938bb1cfabcea306050e245
sutharsan-sivapalan <suth.sivapalan@gmail.com>
2019-01-01 15:21:11 -0800

    Initial commit of all files.

---
+++ scripts/oob-ansible-upgrade.sh
@@ -0,0 +1,4 @@
+sudo apt-get update
+sudo pt-get install -y software-properties-common
+sudo apt-add-repository -y --update ppa:ansible/ansible
+sudo apt-get install -y -o Dpkg::Options::="--force-confold" ansible 
